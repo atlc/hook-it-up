c7dc9b18a39a44e30095e066c4ddedadc627400d
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-05-19 10:15:43 -0500

    Creating template from routing lab to use in hooks lab

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import Films from './api/Films';
+import FilmByID from './api/FilmByID';
+
+class App extends Component {
+    render() {
+        return (
+            <Container className="text-center">
+                <Router>
+                    <>
+                        <Row>
+                            <Link to="/">
+                                <Button variant="dark" style={{margin: "10px"}}>Home</Button>
+                            </Link>
+                            <Link to="/films">
+                                <Button variant="dark" style={{margin: "10px"}}>Films</Button>
+                            </Link>
+                        </Row>
+                        <Row>
+                            <Switch>
+                                <Route exact path="/" component={Logo} />
+                                <Route exact path="/films" component={Films} />
+                                <Route path="/films/:id" component={FilmByID} />
+                            </Switch>
+                        </Row>
+                    </>
+                </Router>
+            </Container>
+        );
+    }
+}
+
+export default App;
